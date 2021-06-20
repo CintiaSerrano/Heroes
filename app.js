7ed3842e86f8cd426eaa7b22119e288585326d3c
emignis <cinivis_ser@live.com.ar>
2021-06-20 20:25:19 -0300

    paso 1

--- app.js
+++ app.js
@@ -2,5 +2,8 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
+app.listen(3030, ()=> {
+    console.log('Servidor corriendo en localhost:3030')
+});
 
 
